1ade1052564ff769d027521fcdb0759ee6f39543
Abhishek Bhatt <abhibhatt587@gmail.com>
2026-01-06 21:00:35 +0530

    Add Upcoming and previous meeting pages and render card in thoese sections.

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -16,7 +16,7 @@ const HomeLayout = ({ children }: { children: ReactNode }) => {
             <Navbar />
             <div className='flex'>
                 <Sidebar />
-                <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 mx-md:pb-14 sm:px-14'>
+                <section className='flex min-h-screen flex-1 px-6 pb-6 pt-28 mx-md:pb-14 sm:px-14'>
                     <div className='w-full'>
                         {children}
                     </div>
--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,3 +1,4 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const page = () => {
@@ -7,6 +8,7 @@ const page = () => {
       <h1 className='text-3xl font-bold'>
         Pervious
       </h1>
+      <CallList type='ended' />
     </section>
   )
 }
